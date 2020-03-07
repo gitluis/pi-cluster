e4cd268cab794fd89670852b4b9ce85654000263
osxluis <luis.rivera9257@gmail.com>
2020-03-07 12:17:38 -0400

    adding pi-cluster commands

---
+++ scripts/cluster_commands.sh
@@ -0,0 +1,46 @@
+# ------------------- #
+# Pi-Cluster Commands #
+# ------------------- #
+
+# return the IP address and mapped hostname of each node in the cluster
+function nodes {
+  grep "master" /etc/hosts | awk '{print $0}' && \
+  grep "worker" /etc/hosts | awk '{print $0}'
+}
+
+# return only the hostname of each worker in the cluster
+function workers {
+  grep "worker" /etc/hosts | awk '{print $2}'
+}
+
+# send a command to be executed by all workers in the cluster
+function master-cmd {
+  for worker in $(workers)
+  do
+    ssh $worker "$@"
+  done
+}
+
+# send a command to be executed by all nodes in the cluster
+function cluster-cmd {
+  master-cmd $@
+  $@
+}
+
+# reboot all nodes in the cluster at the same time
+function cluster-reboot {
+  cluster-cmd shutdown -r now
+}
+
+# shutdown all nodes in the cluster at the same time
+function cluster-shutdown {
+  cluster-cmd shutdown now
+}
+
+# transfer files from one node to all other nodes
+function cluster-scp {
+  for worker in $(workers)
+  do
+    cat $1 | ssh $worker "sudo tee $1" > /dev/null 2>&1
+  done
+}
