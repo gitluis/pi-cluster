451168d181d4483a96b22576b4fe0dbc04c051ec
gitluis <luis.rivera9257@gmail.com>
2020-03-08 22:30:54 -0400

    new command to check workers running

--- scripts/cluster_commands.sh
+++ scripts/cluster_commands.sh
@@ -2,13 +2,16 @@
 # Pi-Cluster Commands #
 # ------------------- #
 
+
 # define cluster user
 USER="pi"
+
 
 # return the user common across all nodes in the cluster
 function cluster-user {
   echo $USER
 }
+
 
 # return the IP address and mapped hostname of each node in the cluster
 function nodes {
@@ -16,18 +19,31 @@ function nodes {
   grep "worker" /etc/hosts | awk '{print $0}'
 }
 
+
 # return only the hostname of each worker in the cluster
 function workers {
   grep "worker" /etc/hosts | awk '{print $2}'
 }
 
+
+# return which nodes are currently up and running
+function workers-up {
+  for worker in $(workers)
+  do
+    ssh $USER@$worker hostname && echo "  is up and running..."
+  done
+}
+
+
 # send a command to be executed by all workers in the cluster
 function master-cmd {
   for worker in $(workers)
   do
+    echo "Running '$@' in $worker as $USER..."
     ssh $USER@$worker "$@"
   done
 }
+
 
 # send a command to be executed by all nodes in the cluster
 function cluster-cmd {
@@ -35,15 +51,18 @@ function cluster-cmd {
   $@
 }
 
+
 # reboot all nodes in the cluster at the same time
 function cluster-reboot {
   cluster-cmd sudo shutdown -r now
 }
 
+
 # shutdown all nodes in the cluster at the same time
 function cluster-shutdown {
   cluster-cmd sudo shutdown now
 }
+
 
 # transfer files from one node to all other nodes
 function cluster-scp {
@@ -52,6 +71,7 @@ function cluster-scp {
     sudo scp $@ $USER@$worker:$@
   done
 }
+
 
 # transfer directories from one node to all other nodes
 function cluster-scpr {
